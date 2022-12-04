21f752fed644fd9a2e807e06cc52bb5b6ea4071d
evencoding <evencoding@gmail.com>
2022-12-04 14:24:23 +0900

    feat: 게임 시작 메시지 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const BaseballController = require('./controller/BaseballController');
+
 class App {
-  play() {}
+  #baseballCtrl;
+
+  constructor() {
+    this.#baseballCtrl = new BaseballController();
+  }
+
+  play() {
+    this.#baseballCtrl.gameStart();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,3 +1,9 @@
-class BaseballController {}
+const OutputView = require('../views/OutputView');
+
+class BaseballController {
+  gameStart() {
+    OutputView.printGameStartMessage();
+  }
+}
 
 module.exports = BaseballController;
