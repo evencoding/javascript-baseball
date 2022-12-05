35de74aaadcf5f942cfe560e805ec9462551bdce
evencoding <evencoding@gmail.com>
2022-12-05 12:51:49 +0900

    refactor: 랜덤 숫자 생성 시 중복을 방지하기 위해 Set을 사용하도록 수정

--- src/GenerateRandomNumbers.js
+++ src/GenerateRandomNumbers.js
@@ -3,13 +3,12 @@ const { GAME_VALUE } = require('./constants');
 
 const GenerateRandomNumbers = {
   generateRandomNumbers() {
-    const computer = [];
-    while (computer.length < GAME_VALUE.LENGTH) {
+    const computer = new Set();
+    while (computer.size < GAME_VALUE.LENGTH) {
       const random = Random.pickNumberInRange(
         GAME_VALUE.MIN_NUMBER,
         GAME_VALUE.MAX_NUMBER
       );
-      if (computer.includes(random)) continue;
       computer.push(random);
     }
 
