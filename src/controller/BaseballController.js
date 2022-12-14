2e8f0ddfeb7b280a2bbc8b6795d1af8d5db93861
evencoding <evencoding@gmail.com>
2022-12-14 22:50:45 +0900

    feat: 1~9 범위의 중복되지 않는 임의의 세자리 수를 생성하는 기능 구현

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -1,6 +1,13 @@
+const BaseballGame = require('../service/BaseballGame');
 const OutputView = require('../views/OutputView');
 
 class BaseballController {
+  #baseballGame;
+
+  constructor() {
+    this.#baseballGame = new BaseballGame();
+  }
+
   startGame() {
     OutputView.printStartMessage();
   }
--- src/service/BaseballGame.js
+++ src/service/BaseballGame.js
@@ -1,3 +1,12 @@
-class BaseballGame {}
+const { generateRandomNumbers } = require('../utils/NumberGenerator');
+
+class BaseballGame {
+  #computerNumbers;
+
+  constructor() {
+    this.#computerNumbers = generateRandomNumbers();
+    console.log(this.#computerNumbers);
+  }
+}
 
 module.exports = BaseballGame;
