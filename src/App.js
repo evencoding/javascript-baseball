f24cea74b833b2570d3a5da415df7bc947510c55
evencoding <evencoding@gmail.com>
2022-12-14 22:41:16 +0900

    feat: 뼈대 코드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const BaseballController = require('./controller/BaseballController');
+
 class App {
-  play() {}
+  #baseballCtrl;
+
+  constructor() {
+    this.#baseballCtrl = new BaseballController();
+  }
+
+  play() {
+    this.#baseballCtrl.startGame();
+  }
 }
 
 const app = new App();
---
+++ src/controller/BaseballController.js
@@ -0,0 +1,5 @@
+class BaseballController {
+  startGame() {}
+}
+
+module.exports = BaseballController;
