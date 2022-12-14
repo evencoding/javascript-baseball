058e5290b417c4967b2363b218d69ac3354e5b4a
evencoding <evencoding@gmail.com>
2022-12-14 13:34:38 +0900

    feat: 메인 화면의 기능을 입력하는 기능 구현

--- src/controller/SubwayController.js
+++ src/controller/SubwayController.js
@@ -1,9 +1,30 @@
+const InputView = require('../views/InputView');
 const OutputView = require('../views/OutputView');
+
+const FEATURES = {
+  MAIN: 'main',
+  ROUTE: 'route',
+};
 
 class SubwayController {
   randerMainPage() {
     OutputView.printMainPage();
+    this.#inputFeature(FEATURES.MAIN);
   }
+
+  #inputFeature(type) {
+    if (type === FEATURES.MAIN) {
+      InputView.askFeature(this.#validateMainFeature.bind(this));
+      return;
+    }
+    InputView.askFeature(this.#validateRouteFeature.bind(this));
+  }
+
+  #validateMainFeature(feature) {
+    console.log(feature);
+  }
+
+  #validateRouteFeature(feature) {}
 }
 
 module.exports = SubwayController;
--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,5 +1,13 @@
 const { Console } = require('@woowacourse/mission-utils');
 
-const InputView = {};
+const MESSAGE = {
+  ASK_FEATURE: '\n## 원하는 기능을 선택하세요.\n',
+};
+
+const InputView = {
+  askFeature(callback) {
+    Console.readLine(MESSAGE.ASK_FEATURE, callback);
+  },
+};
 
 module.exports = InputView;
