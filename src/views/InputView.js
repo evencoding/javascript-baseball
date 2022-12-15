7ba76f57305375714f6dd88974b0d884e3915d7c
evencoding <evencoding@gmail.com>
2022-12-15 13:38:14 +0900

    feat: 자동차의 이름 입력, 에러 메시지를 출력하는 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,3 +1,14 @@
-const InputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+const Validator = require('../utils/Validator');
+
+const MESSAGE = {
+  ASK_CARS_NAME: '자동차 이름을 5자 이하 콤마로 구분하여 입력해주세요.\n',
+};
+
+const InputView = {
+  askCarsName(callback) {
+    Console.readLine(MESSAGE.ASK_CARS_NAME, callback);
+  },
+};
 
 module.exports = InputView;
--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,3 +1,17 @@
-const OutputView = {};
+const { Console } = require('@woowacourse/mission-utils');
+
+const MESSAGE = {
+  GAME_START: '자동차 경주 게임을 시작합니다.',
+};
+
+const OutputView = {
+  printErrorMessage(message) {
+    Console.print(message);
+  },
+
+  printGameStartMessage() {
+    Console.print(MESSAGE.GAME_START);
+  },
+};
 
 module.exports = OutputView;
