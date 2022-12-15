6a8e0a5d4564db4096378474504b789a57eb5d9f
evencoding <evencoding@gmail.com>
2022-12-15 13:21:03 +0900

    feat: 0~9 사이의 임의의 값을 반환하는 기능 구현

--- src/utils/RandomNumberGenerator.js
+++ src/utils/RandomNumberGenerator.js
@@ -1,3 +1,9 @@
-const RandomNumberGenerator = {};
+const { Random } = require('@woowacourse/mission-utils');
+
+const RandomNumberGenerator = {
+  generateRandomNumber() {
+    return Random.pickNumberInRange(0, 9);
+  },
+};
 
 module.exports = RandomNumberGenerator;
